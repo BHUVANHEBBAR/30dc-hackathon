b8d9d1ecee3b0fcfe9474b056ffc7e22e709e361
PKM <hrithikn456@gmail.com>
2024-04-22 21:57:00 +0530

    Connection string added

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 
+const dburl="mongodb+srv://tractorbuddyapp:CFqW6wPM91vcbuNZ@cluster0.346nge9.mongodb.net/Nursery?retryWrites=true&w=majority&appName=Cluster0"
+mongoose
+	.connect(dburl)
+	.then(() => {
+		console.log("connected to db");
+	})
+	.catch((error) => {
+		console.error("Error signing up:", error);
+		res.status(500).send("Internal Server Error");
+	});
 
 const app = express();
 const port = 3000;
