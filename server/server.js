55a6c95f70fd120ec082f1d6de41ad7d0ba31cb0
bhuzkashi <bhuvantgh@gmail.com>
2024-05-02 20:42:40 +0530

    vercel-test

--- server/server.js
+++ server/server.js
@@ -46,18 +46,33 @@ const transporter = nodemailer.createTransport({
         city: req.body.city,
         date: req.body.date
       };
-      contactus.create(formData);
+     
   
       // Prepare the email message
       const mailOptions = {
         from: 'tinybudsaca.edu.in',
         to: req.body.email,
         subject: 'Form Submission Confirmation',
-        text: `Thank you for submitting the form. Your form data is as follows:\n\nFirst Name: ${formData.first_name}\nLast Name: ${formData.last_name}\nKids Name: ${formData.kids_name}\nEmail: ${formData.email}\nCity: ${formData.city}\nDate: ${formData.date.substring(0, 10)} \nWe'll contact you soon.`,
+        text: `Thank you for submitting the form. Your form data is as follows:\n\nFirst Name: ${formData.first_name}\nLast Name: ${formData.last_name}\nKids Name: ${formData.kids_name}\nEmail: ${formData.email}\nCity: ${formData.city}\n \nWe'll contact you soon.`,
       };
-  
-      // Send the email
-      await transporter.sendMail(mailOptions);
+      contactus.findOne({email:formData.email}).exec().then((users)=>{
+        if(!users){
+          contactus.create(formData);
+          transporter.sendMail(mailOptions);
+        }
+        else{
+          const mailOptions = {
+            from: 'tinybudsaca.edu.in',
+            to: req.body.email,
+            subject: 'Form Submission Confirmation',
+            text: `It seems you have already reached us. We will get back to you soon.
+            Thank you for submitting the form. Your form data is as follows:\n\nFirst Name: ${formData.first_name}\nLast Name: ${formData.last_name}\nKids Name: ${formData.kids_name}\nEmail: ${formData.email}\nCity: ${formData.city}\n\nWe'll contact you soon.`,
+          };
+          transporter.sendMail(mailOptions);
+
+        }
+      });
+      
   
       // Send a response indicating success
       res.send('Form data saved and email sent successfully');
@@ -65,6 +80,16 @@ const transporter = nodemailer.createTransport({
       // Handle any errors that occur during saving or sending the email
       console.error('Error saving form data or sending email:', error);
       res.status(500).send('An error occurred while saving the form data or sending the email');
+    }
+  });
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await contactus.find().lean();
+      res.json(contacts);
+      console.log(contacts);
+    } catch (error) {
+      console.error("Error fetching contact data:", error);
+      res.status(500).json({ message: "An error occurred while fetching contact data" });
     }
   });
 
